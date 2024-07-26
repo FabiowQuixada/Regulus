40484bb41efd1298e7bd203ad2a4a41e86fb2063
Fabiow Quixadá <ftquixada@gmail.com>
2024-07-26 11:06:48 -0300

    Use Single Routes File

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const routes  = require('./routes');
 
 const app = express();
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const cartRoutes = require('./routes/cart');
-const errorsController = require('./controllers/errors');
-const homeController = require('./controllers/home');
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/admin', adminRoutes);
-app.use(cartRoutes);
-app.use(shopRoutes);
-
-app.use(homeController.getHome);
-app.use(errorsController.get404);
+app.use('/', routes);
 
 app.listen(3000);
---
+++ routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const homeController     = require('./controllers/home');
+const productsController = require('./controllers/products');
+const errorsController   = require('./controllers/errors');
+const cartController     = require('./controllers/cart');
+
+const router = express.Router();
+
+router.get('/', homeController.getHome);
+router.get('/admin/add-product', productsController.getAddProduct);
+router.post('/admin/add-product', productsController.postAddProduct);
+router.get('/products', productsController.getProducts);
+router.get('/cart', cartController.getCart);
+router.get(errorsController.get404);
+
+module.exports = router;
