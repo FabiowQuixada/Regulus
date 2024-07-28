30d3de9fc388071effe2940b7b1544e9ad3a8a86
Fabiow Quixadá <ftquixada@gmail.com>
2024-07-28 13:13:19 -0300

    Get Product Data from Database

--- controllers/products.js
+++ controllers/products.js
@@ -17,27 +17,29 @@ const postAddProduct = (req, res, next) => {
 };
 
 const getProducts = (req, res, next) => {
-    Product.fetchAll(productList => {
-        res.render('shop/product-list', {
-            productList,
-            pageTitle: 'Shop',
-            path : '/products'
-        });
-    });
+    Product.fetchAll()
+        .then(([productList]) => {
+            res.render('shop/product-list', {
+                productList,
+                pageTitle: 'Shop',
+                path : '/products'
+            });
+        })
+        .catch((err) => { console.log(err); });
 };
 
 const getProduct = (req, res, next) => {
     const productId = req.params.productId;
 
-    Product.findById(productId, product => {
-        console.log(product);
-
-        res.render('shop/product-details', {
-            product,
-            pageTitle: 'Shop',
-            path : '/products'
-        });
-    });
+    Product.findById(productId)
+        .then(([productList, fieldData]) => {
+            res.render('shop/product-details', {
+                product : productList[0],
+                pageTitle: 'Shop',
+                path : '/products'
+            });
+        })
+        .catch(err => { console.log(err);});
 };
 
 module.exports = {
--- models/product.js
+++ models/product.js
@@ -1,42 +1,13 @@
-const fs = require('fs');
-const path = require('path');
-const pathUtils = require('../util/path');
+const dbUtils = require('../util/database');
 
-const productsDatabasePath = path.join(pathUtils.root, 'data', 'products.json');
-
-const getProductListFromFile = (cb) => {
-    fs.readFile(productsDatabasePath, (err, fileContent) => {
-        if (!err) {
-            return cb(JSON.parse(fileContent));
-        }
-
-        return cb([]);
-    });
-};
+const db = dbUtils.poolPromisse;
 
 module.exports = class Product {
-    constructor(title) {
-        this.title = title;
+    static fetchAll() {
+        return db.execute('SELECT * from products');
     }
 
-    save() {
-        getProductListFromFile((productList) => {
-            productList.push(this);
-
-            fs.writeFile(productsDatabasePath, JSON.stringify(productList), err2 => {
-                console.log(err2);
-            });
-        });
-    }
-
-    static fetchAll(cb) {
-        getProductListFromFile(cb);
-    }
-
-    static findById(id, cb) {
-        getProductListFromFile(productList => {
-            const product = productList.find(p => p.id == id);
-            cb(product);
-        });
+    static findById(id) {
+        return db.execute(`SELECT * from products where id = ${id}`);
     }
 };
